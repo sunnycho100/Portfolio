4ec29c8a6c16eb656920d6b69a425838d505b153
sunnycho100 <studysung1880@gmail.com>
2025-12-01 00:53:39 -0600

    sever connecting

---
+++ server/prisma/migrations/20251201065207_init_comments/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `relation` VARCHAR(191) NULL,
+    `message` VARCHAR(500) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
